ee8a1c048f977f37357102713ef90bfc7e951d86
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-11-18 18:06:12 -0800

    do some better argument parsing using argparse
    Argparse got added into the stdlib in 2.7 and 3.2, the latter of which is
    currently still in alpha.  For other versions, you can install it from PyPi[0].
    
    [0]: http://pypi.python.org/pypi/argparse

--- waypoint.py
+++ waypoint.py
@@ -5,6 +5,7 @@ try:
 except ImportError:
 	# renamed in py3k
 	from configparser import SafeConfigParser
+from argparse import ArgumentParser
 
 class Waypoints(object):
 	def __init__(self):
@@ -12,12 +13,14 @@ class Waypoints(object):
 		self.configPath = os.path.expanduser('~/.config/waypoint/waypoint.config')
 		self.config.read(self.configPath)
 	
-	def add(self, path, name='default', section='DEFAULT'):
+	def add(self, name, section):
+		path = os.getcwd()
+		
 		self.config.set(section, name, path)
 		with open(self.configPath, 'w') as configFile:
 			self.config.write(configFile)
 	
-	def goto(self, name='default', section='DEFAULT'):
+	def goto(self, name, section):
 		path = self.config.get(section, name)
 		#os.chdir(path)
 		with open(os.path.expanduser('~/.config/waypoint/scratch.sh'), 'w') as file:
@@ -26,7 +29,25 @@ class Waypoints(object):
 if __name__=='__main__':
 	waypoints = Waypoints()
 	
-	if sys.argv[1] == 'add':
-		waypoints.add(os.getcwd())
-	elif sys.argv[1] == 'goto':
-		waypoints.goto()
+	parser = ArgumentParser(description='Teleport around your shell.')
+	subparsers = parser.add_subparsers()
+	
+	parser_add = subparsers.add_parser('add')
+	parser_add.add_argument('name', default='default', nargs='?')
+	parser_add.add_argument('section', default='DEFAULT', nargs='?')
+	parser_add.set_defaults(func=waypoints.add)
+	
+	parser_goto = subparsers.add_parser('goto')
+	parser_goto.add_argument('name', default='default', nargs='?')
+	parser_goto.add_argument('section', default='DEFAULT', nargs='?')
+	parser_goto.set_defaults(func=waypoints.goto)
+	
+	# Because I didn't want to pass an argparse.Namespace to the functions and
+	# have to access everything through args.foo, we pull out the values as a
+	# dictionary and splat 'em in.
+	# (Shallow-)Copying the dict is necessary, since we want to avoid passing
+	# the going-to-be-called function in as a parameter to itself.
+	args = parser.parse_args().__dict__
+	function = args['func']
+	del args['func']
+	function(**args)
